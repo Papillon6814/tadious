e0ffdafd4bbe9e1d720ef8466692e69c8e9c6da1
Papillon6814 <so1rou.k@gmail.com>
2021-08-04 19:22:09 +0900

    feat: client

---
+++ tadious-cli/target/debug/deps/tadious_cli-03e018b2c368ca76.d
@@ -0,0 +1,5 @@
+/Users/papillon/Documents/GitHub/tadious/tadious-cli/target/debug/deps/tadious_cli-03e018b2c368ca76.rmeta: src/main.rs
+
+/Users/papillon/Documents/GitHub/tadious/tadious-cli/target/debug/deps/tadious_cli-03e018b2c368ca76.d: src/main.rs
+
+src/main.rs:
---
+++ tadious-cli/target/debug/deps/tadious_cli-85f3f5455638f971.d
@@ -0,0 +1,5 @@
+/Users/papillon/Documents/GitHub/tadious/tadious-cli/target/debug/deps/tadious_cli-85f3f5455638f971.rmeta: src/main.rs
+
+/Users/papillon/Documents/GitHub/tadious/tadious-cli/target/debug/deps/tadious_cli-85f3f5455638f971.d: src/main.rs
+
+src/main.rs:
